8bfee042abe1b70027ad97e8da1dc4e36ba9f8e3
Donato-Carola <don94@live.it>
2023-12-20 13:06:07 +0100

    struttura header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup>
-
-</script>
-
 <template>
-  
+  <AppHeader/>
+  <AppMain/>
+ 
 </template>
 
-<style lang="scss" scoped>
+
+<script > 
+/*import { store } from "./js/store.js";
+import axios from "axios";*/
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  },
+
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+
+
+<style lang="scss" >
 @use "./styles/general.scss" as *;
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,39 @@
-<template lang="">
-    <div>
-        
+<template >
+
+  <header class="bg-black">
+    <section class="d-flex  justify-content-between align-items-center container pt-3 pb-3 ">
+    <div class="text-uppercase text-danger">boolfix</div>
+    <div class="input-group w-25">
+      <button
+        class="btn btn-outline-secondary"
+        type="button"
+        id="button-addon1"
+      >
+        Button
+      </button>
+      <input
+        type="text"
+        class="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+      />
     </div>
+  </section>
+  </header>
 </template>
+
 <script>
 export default {
-    
-}
+    name:'AppHeader'
+};
 </script>
+
+
+
+
 <style lang="scss" scoped>
-    
+ header{
+    justify-content: space-between;
+ }
 </style>
