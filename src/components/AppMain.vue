799992084d79428ed866235ea1a9ba3fd4009b12
Donato-Carola <don94@live.it>
2023-12-20 15:07:32 +0100

    creazione component per avere le carte all'interno

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,23 @@
 <template lang="">
-    <div>
+    <main>
+      <CardsWrapper/>
+
+
+    </main>
         
-    </div>
+    
 </template>
 <script>
+import CardsWrapper from './CardsWrapper.vue';
 export default {
-    
+    name:'AppMain',
+    components:{
+        CardsWrapper,
+}
 }
 </script>
 <style lang="scss" scoped>
-    
+    main{
+        background-color: rgb(67, 67, 67);
+    }
 </style>
---
+++ src/components/CardsWrapper.vue
@@ -0,0 +1,14 @@
+<template >
+    <section class="container p-5">
+
+        
+    </section>
+</template>
+<script>
+export default {
+    name:'CardsWrapper'
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
