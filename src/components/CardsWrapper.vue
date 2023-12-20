ca4d97cca6990cfebbe86a664426621392bad123
Donato-Carola <don94@live.it>
2023-12-20 16:44:12 +0100

    chiamata axios

--- src/components/CardsWrapper.vue
+++ src/components/CardsWrapper.vue
@@ -1,12 +1,45 @@
 <template >
     <section class="container p-5">
+      <div class="row">
+        <singleCardFilm v-for="card in cardList" :key="card.id" :card="card"/>
 
+      </div>
         
     </section>
 </template>
 <script>
+import singleCardFilm from './singleCardFilm.vue';
+import axios from 'axios';
 export default {
-    name:'CardsWrapper'
+
+    data() {
+        return {
+            cardList:[],
+            apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=03577983b6885d7b8f4dfcc7600081db&query=ritorno+al+futuro'
+        }
+    },
+
+    name:'CardsWrapper',
+    components:{
+       singleCardFilm
+    },
+
+    methods:{
+        getCards(){
+          axios.get(this.apiUrl)
+          .then((response)=>{
+            console.log(response);
+            this.cardList = response.data.results
+
+          })
+          .catch(function(error){
+            console.log(error);
+          })
+        }
+    },
+    created(){
+        this.getCards();
+    },
 }
 </script>
 <style lang="scss" scoped>
---
+++ src/components/singleCardFilm.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <article class="w-100">
+        <h3>
+            {{card.original_title}}
+        </h3>
+    </article>
+</template>
+<script>
+export default {
+    props:{
+           card : {
+            type: Object,
+            required: true,
+           }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
